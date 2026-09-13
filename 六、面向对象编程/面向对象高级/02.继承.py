4ce6a2bd47f415c94784baa4ef30d07d9f694fb6
Orion.Guan <shanyue0828@gmail.com>
2026-09-13 12:25:19 +0800

    feat(面向对象高级): 多继承的方法解析顺序（MRO）

--- 六、面向对象编程/面向对象高级/02.继承.py
+++ 六、面向对象编程/面向对象高级/02.继承.py
@@ -10,6 +10,30 @@ class 子类名(父类名):
 说明:
 1、定义的类如果没有显式使用括号指定要继承的父类，则默认继承自object类，即所有类都是object类的子类。
 2、子类只能继承父类中所有公有的成员属性和方法，无法继承父类中的私有属性和方法。
+
+
+1.1 多继承
+多继承是指一个子类可以继承多个父类（Java中是单继承），从而拥有所有父类中的非私有的成员属性和方法。
+
+class 子类名(父类名1,父类名2,父类名3):
+    def __init__(self):
+        super().__init__()      # 调用父类1的__init__方法
+        父类名2.__init__(self)   # 调用父类2的__init__方法
+        父类名3.__init__(self)   # 调用父类3的__init__方法
+        self.fly = True         # 新增一个子类自己的实例属性fly
+    pass
+
+MRO方法解析顺序:
+背景: 在多继承中，子类待调用的方法在自身以及多个父类中都有，优先使用哪个类中的方法？ 答：Python会遵循MRO原则。可以通过"类名.mro() | 类名.__mro__"方法查看。
+顺序：
+    1、子类自己的方法
+    2、父类1的方法（不会向上找父类1的父类的方法）
+    3、父类2的方法
+    4、父类3的方法
+    5、object类的方法
+
+
+
 """
 
 
@@ -35,11 +59,15 @@ class Car:
     def run(self):
         print(f"{self.name}父类汽车开始启动")
 
+    def order(self):
+        print("Car.order() 执行了!")
+
 
 class FuelCar(Car):
     """
     汽油车
     """
+
     def info(self):
         # 调用父类中的方法(方式一: super().父类方法名())
         string = super().info()
@@ -60,10 +88,30 @@ class ElectricCar(Car):
     def info(self):
         self.run()
         return f"{self.name}的 {self.typenum} 汽车！"
+
+    def order(self):
+        print("ElectricCar.order() 执行了!")
     pass
 
-car1 = FuelCar("小米yu7", "灰白色", 45, "燃油车")
-print(car1.info())
 
-car2 = ElectricCar("特斯拉", "白色", 120, "电动汽车")
-print(car2.info())
+class HunDongCar(FuelCar,ElectricCar):
+    """
+    混动汽车
+    """
+
+# car1 = FuelCar("小米yu7", "灰白色", 45, "燃油车")
+# print(car1.info())
+#
+# car2 = ElectricCar("特斯拉", "白色", 120, "电动汽车")
+# print(car2.info())
+
+
+
+# 测试在多继承中，多个父类都有相同的待调用方法，优先使用哪个类中的方法
+hdc = HunDongCar('比亚迪','red',56,'A888')
+# hdc.info()
+print(HunDongCar.mro())    # 查看HRO方法解析顺序(方式一)
+print(HunDongCar.__mro__)   # 查看MRO方法解析顺序(方式二)
+print(hdc.__dict__)     # 查看hdc对象的所有属性
+
+hdc.order()  # 调用的是ElectricCar中的order方法
